e88f28c1121e7d4a8dc2db0cc33d453f2884d762
Ed J <mohawk2@users.noreply.github.com>
2020-09-17 14:41:18 +0100

    1.005

--- lib/PDL/Transform/Color.pm
+++ lib/PDL/Transform/Color.pm
@@ -337,7 +337,7 @@ package PDL::Transform::Color;
 use PDL::Core ':Internal';  # load "topdl" (internal routine)
 
 @ISA = ( 'Exporter', 'PDL::Transform' );
-our $VERSION = '1.004';
+our $VERSION = '1.005';
 $VERSION = eval $VERSION;
 
 BEGIN {
